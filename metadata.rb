296a246119a81e24904ba2dcf44f8e8af6724a64
Ryan Larson <ryan.mango.larson@gmail.com>
2017-03-22 10:07:19 -0700

    Initial cookbook structure

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'fisheye'
+maintainer       'Ryan Larson'
+maintainer_email 'ryan.mango.larson@gmail.com'
+license          'MIT'
+description      'Installs/Configures atlassian fisheye'
+long_description description
+version          '0.1.0'
